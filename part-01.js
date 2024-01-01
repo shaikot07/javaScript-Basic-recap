4b24e58cd0764d9489850091c0b42f774fc3c944
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-01 23:19:13 +0600

    Write a function that takes an object with two properties as argument and return  return the value of the property with key countr

--- part-01.js
+++ part-01.js
@@ -1,12 +1,12 @@
 function findAddress(obj) {
-      if(typeof obj !=='object'){
+      if (typeof obj !== 'object') {
             return 'please Provide me a valid Object'
-      }else{
-            const street =obj.street ||'__';
-            const house = obj.house ||'__';
-            const society =obj.society ||'__'
+      } else {
+            const street = obj.street || '__';
+            const house = obj.house || '__';
+            const society = obj.society || '__'
 
-            return street+','+house+','+society;
+            return street + ',' + house + ',' + society;
       }
 }
 
@@ -17,39 +17,48 @@ const address = {
       street: 10,
       house: '15A',
       society: 'Earth Perfect'
-    }
+}
 
 
-console.log(findAddress(address));    
+console.log(findAddress(address));
 
 // problem number 02 
 
 function cubeNumber(number) {
       if (typeof number !== "number") {
-        return "Invalid input";
+            return "Invalid input";
       }
-      else{
-        return number * number * number;
+      else {
+            return number * number * number;
       }
-      
-    }
+
+}
 
 console.log(cubeNumber(3));
 
 function canPay(changeArray, totalDue) {
-      if(changeArray.length ==0){
-        return 'invalid array'
+      if (changeArray.length == 0) {
+            return 'invalid array'
       }
       let totalChange = 0;
-      for(let i =0;i<changeArray.length;i++){
-        totalChange += changeArray[i];
-         
-        }
-      if(totalChange >= totalDue){
-        return true
-      }else{
-        return false
+      for (let i = 0; i < changeArray.length; i++) {
+            totalChange += changeArray[i];
+
       }
-    }
-const array = [2,3,4,6,7,];
-console.log( canPay(array));
+      if (totalChange >= totalDue) {
+            return true
+      } else {
+            return false
+      }
+}
+const array = [2, 3, 4, 6, 7,];
+console.log(canPay(array));
+
+// Write a function that takes an object with two properties as argument
+// It should return the value of the property with key country
+function myFunction(obj) {
+      const result =obj.country;
+      return result
+}
+
+console.log(myFunction({ continent: 'Asia', country: 'Japan' }));
