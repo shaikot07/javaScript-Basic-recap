90f9820b6e7b6529a56ea515446629080bdd04a7
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-03 00:22:56 +0600

    print the ODD number

--- part-02.js
+++ part-02.js
@@ -24,3 +24,14 @@ for(let i= start; i <= end; i++){
         console.log(i)
     }
 }
+
+/* Write the JS Code to print all ODD numbers between start and end.*/ 
+const start2 = 10;
+const end2 = 20;
+
+
+for(let i= start; i <= end; i++){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+}
