6ea42d7a7d95f13146bbe85d245019a78f396f89
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-03 01:03:07 +0600

    whter problems

--- part-02.js
+++ part-02.js
@@ -14,26 +14,26 @@ C = 20;
 
 console.log(C + B + A);
 
-/* Write the JS Code to print all even numbers between start and end.*/ 
+/* Write the JS Code to print all even numbers between start and end.*/
 const start = 10;
 const end = 20;
 
 
-for(let i= start; i <= end; i++){
-    if(i % 2 === 0){
-        console.log(i)
-    }
+for (let i = start; i <= end; i++) {
+      if (i % 2 === 0) {
+            console.log(i)
+      }
 }
 
-/* Write the JS Code to print all ODD numbers between start and end.*/ 
+/* Write the JS Code to print all ODD numbers between start and end.*/
 const start2 = 10;
 const end2 = 20;
 
 
-for(let i= start; i <= end; i++){
-    if(i % 2 !== 0){
-        console.log(i)
-    }
+for (let i = start; i <= end; i++) {
+      if (i % 2 !== 0) {
+            console.log(i)
+      }
 }
 
 console.log(i);
@@ -44,7 +44,7 @@ const numbers = [10, 15, 20, 30];
 let sum = 0;
 
 for (let i = 0; i < numbers.length; i++) {
-    sum = sum + numbers[i];
+      sum = sum + numbers[i];
 }
 console.log(sum);
 
@@ -57,22 +57,30 @@ const weight = [30, 55, 90, 102, 65];
 for (let i = 0; i < height.length; i++) {
       // Calculate BMI
       const bmi = weight[i] / (height[i] ** 2);
-  
+
       // Display BMI value with two decimal places
       const formattedBMI = bmi.toFixed(2);
-  
+
       // Determine BMI category
       let category = "";
       if (bmi < 18.5) {
-          category = "Underweight";
+            category = "Underweight";
       } else if (bmi >= 18.5 && bmi <= 24.9) {
-          category = "Normal Weight";
+            category = "Normal Weight";
       } else if (bmi >= 25 && bmi <= 29.9) {
-          category = "Overweight";
+            category = "Overweight";
       } else {
-          category = "Obese";
+            category = "Obese";
       }
-  
+
       // Display the result
       console.log(`${formattedBMI} ${category}`);
-  }
+}
+
+// Write a function that takes a string as argument
+// Create an object that has a property with key 'key' and a value equal to the string
+// Return the object
+function myFunction (a) {
+
+      return
+}
