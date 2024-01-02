a3e85964efc5ab15db6b349c9843ac66b019e7a5
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-03 00:58:21 +0600

    solve the intersting problem

--- part-02.js
+++ part-02.js
@@ -35,3 +35,42 @@ for(let i= start; i <= end; i++){
         console.log(i)
     }
 }
+
+const numbers = [10, 15, 20, 30];
+/* Iterate over this array and find the total sum. */
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+}
+console.log(sum);
+
+const height = [1.70, 1.65, 1.80, 1.60, 1.75];
+const weight = [30, 55, 90, 102, 65];
+
+/* Using these 5 users find the BMI */
+/* Update your code below this line */
+
+for (let i = 0; i < height.length; i++) {
+      // Calculate BMI
+      const bmi = weight[i] / (height[i] ** 2);
+  
+      // Display BMI value with two decimal places
+      const formattedBMI = bmi.toFixed(2);
+  
+      // Determine BMI category
+      let category = "";
+      if (bmi < 18.5) {
+          category = "Underweight";
+      } else if (bmi >= 18.5 && bmi <= 24.9) {
+          category = "Normal Weight";
+      } else if (bmi >= 25 && bmi <= 29.9) {
+          category = "Overweight";
+      } else {
+          category = "Obese";
+      }
+  
+      // Display the result
+      console.log(`${formattedBMI} ${category}`);
+  }
