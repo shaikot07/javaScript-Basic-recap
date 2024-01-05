ae4276272876a4978f5814d343973782d53833f4
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-05 21:52:02 +0600

    check prime number

--- pat-04.js
+++ pat-04.js
@@ -30,3 +30,19 @@ function calculateFactorial(num) {
     
     const factorialOf5 = calculateFactorial(5);
     console.log(factorialOf5); 
+
+//     Problem: Check if a Number is Prime
+
+function isPrime(num) {
+      if (num <= 1) return false;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+    
+    // Example usage:
+    console.log(isPrime(7))
+    console.log(isPrime(14))
