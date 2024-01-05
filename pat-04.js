942e3d8a6a22303a8021577588c2da7b8a17ec90
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-05 20:10:20 +0600

    Find the Largest Number in an Array

--- pat-04.js
+++ pat-04.js
@@ -7,3 +7,13 @@ function reverseString(str) {
 const originalString = 'Hello, World!';
 const reversedString = reverseString(originalString);
 console.log(reversedString);
+
+// 2. Problem: Find the Largest Number in an Array
+
+function findLargestNumber(arr) {
+      return Math.max(...arr);
+    }
+    
+    const numbers = [2, 8, 1, 5, 9];
+    const largestNumber = findLargestNumber(numbers);
+    console.log(largestNumber);
