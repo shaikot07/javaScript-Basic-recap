0c348c83b20ce5e486fa12c4c52fc17b98e6ebb8
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-10 22:23:03 +0600

    Count Vowel in a String

--- part-05.js
+++ part-05.js
@@ -10,10 +10,10 @@ const textThree = `me shaikot`;
 console.log(textOne, textTwo, textThree);
 
 // connect 2 strings together. In the code below we use the Addition (+) operator to concatenate textOne and textTwo
-const textOne = 'Hello';
-const textTwo = 'World';
-const combined = textOne + ' ' + textTwo;
-console.log(combined);
+// const textOne = 'Hello';
+// const textTwo = 'World';
+// const combined = textOne + ' ' + textTwo;
+// console.log(combined);
 
 // Problem: Given a string, write a function to reverse it.
 
@@ -24,3 +24,14 @@ function reverseString(str) {
     // test case for this problem
     const reversed = reverseString('Hello');
     console.log(reversed); 
+
+
+//     Problem 2: Count Vowel in a String
+    function countVowel(str) {
+      const vowels = 'aeiouAEIOU';
+      return str.split('').filter(char => vowels.includes(char)).length;
+    }
+    
+    // Example usage:
+    const vowelCount = countVowel('Hello World');
+    console.log(vowelCount); 
