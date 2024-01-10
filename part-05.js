fa4ecf79a2567d22adb8d16258953ab1b53ea8ae
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-10 22:01:42 +0600

    connect 2 strings together. In the code below we use the Addition (+) operator to concatenate textOne and textTwo

--- part-05.js
+++ part-05.js
@@ -8,3 +8,9 @@ const textOne = 'Hello, ';
 const textTwo = "it's ";
 const textThree = `me shaikot`;
 console.log(textOne, textTwo, textThree);
+
+// connect 2 strings together. In the code below we use the Addition (+) operator to concatenate textOne and textTwo
+const textOne = 'Hello';
+const textTwo = 'World';
+const combined = textOne + ' ' + textTwo;
+console.log(combined);
