d1fe0dd769c5c32a09855aea98587d8990e76508
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-10 21:45:00 +0600

    there defrent way to declery  string (single '', dubble qutation and backTik

--- part-05.js
+++ part-05.js
@@ -2,3 +2,9 @@
 
 const text = 'hello world';
 console.log('The value of text is: ' + text);
+
+// there defrent way to declery  string (single '', dubble " " and backtik ``)
+const textOne = 'Hello, ';
+const textTwo = "it's ";
+const textThree = `me shaikot`;
+console.log(textOne, textTwo, textThree);
