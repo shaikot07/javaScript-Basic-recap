ba26e63a1404d790bb5bd64f2f01cc4044d6b828
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-12 22:16:40 +0600

    remove duplycate item in array

--- part-06.js
+++ part-06.js
@@ -2,17 +2,22 @@
 
 function sumArray(arr) {
       return arr.reduce((sum, num) => sum + num, 0);
-    }
-    
-    const numbers = [1, 2, 3, 4, 5];
-    console.log(sumArray(numbers));
+}
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(sumArray(numbers));
 
 //     Problem 2: Find Maximum Element
 
 function findMax(arr) {
       return Math.max(...arr);
-    }
-    
-    const number = [3, 7, 2, 8, 5,25];
-    const result =findMax(number)
-    console.log(result);
+}
+
+const number = [3, 7, 2, 8, 5, 25];
+const result = findMax(number)
+console.log(result);
+
+//     Problem 3: Remove Duplicates
+function removeDuplicates(arr) {
+      return [...new Set(arr)];
+}
