991ce12239b5efb6e3598436056bc2f2c3868464
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-13 19:45:50 +0600

    generate a random integer number

--- part-07.js
+++ part-07.js
@@ -8,6 +8,23 @@ let base_convert = function(number, initial_base, change_base) {
        return parseInt(number + '', initial_base).toString(change_base);
    }
    
-   console.log(base_convert('E164',19,6));
-   console.log(base_convert(1000,2,8));
-   console.log(base_convert(1546,45,7));
+//    console.log(base_convert('E164',19,6));
+//    console.log(base_convert(1000,2,8));
+//    console.log(base_convert(1546,45,7));
+
+//    JavaScript function to generate a random integer.
+
+let rand = function(min, max) {
+      if (min==null && max==null)
+        return 0;    
+      
+      if (max == null) {
+          max = min;
+          min = 0;
+        }
+        return min + Math.floor(Math.random() * (max - min + 1));
+      };
+     console.log(rand(20,1));
+     console.log(rand(1,10));
+     console.log(rand(6));
+     console.log(rand());
