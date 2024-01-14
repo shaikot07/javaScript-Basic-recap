2a632634d78423dbabcdd9e6c2fcb9a38d9a2331
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-14 22:39:26 +0600

    syntext and  if else practices

--- part-08.js
+++ part-08.js
@@ -1,12 +1,22 @@
 const twoSum = (array, goal) => {
       let indexes = [];
       for (let i = 0; i < array.length; i++) {
-        for (let j = i + 1; j < array.length; j++) {
-          if (array[i] + array[j] === goal) {
-            indexes.push(i);
-            indexes.push(j);
-          }
-        }
+            for (let j = i + 1; j < array.length; j++) {
+                  if (array[i] + array[j] === goal) {
+                        indexes.push(i);
+                        indexes.push(j);
+                  }
+            }
       }
       return indexes;
-    };
+};
+
+//  syntext and  if else practices 
+let shoppingDone = false;
+let childsAllowance;
+
+if (shoppingDone === true) {
+      childsAllowance = 10;
+} else {
+      childsAllowance = 5;
+}
