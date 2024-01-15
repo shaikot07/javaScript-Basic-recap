9a936a6feb0bec02b277ddddaf00a8fca8119b8a
shaikot07 <saifulislamshaikat007@gmail.com>
2024-01-15 21:20:35 +0600

    exploring redux by anisul islam

---
+++ part-09-exploring-redux.js
@@ -0,0 +1,19 @@
+// defining constants 
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const ADD_USER = "ADD_USER";
+
+// state 
+const initialCounterState ={
+      count : 0,
+};
+const initialUsersState ={
+      user :[{name: "saiful islam shaikat"}]
+};
+// action - Object- type, payload
+const incrementCounter =()=>{
+      return {
+            type: INCREMENT,
+      };
+};
