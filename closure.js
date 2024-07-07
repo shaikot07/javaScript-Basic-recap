88041f3f9c16535f870465583e6cbb8ab520a074
shaikot07 <saifulislamshaikat007@gmail.com>
2024-07-07 22:33:22 +0600

    updated  test case 03

--- closure.js
+++ closure.js
@@ -70,7 +70,11 @@ function fibonacci(n) {
     // Base cases
     if (n === 0) {
         return 0;
-    
+    } else if (n === 1) {
+        return 1;
+    }
+    // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 // Example usage
